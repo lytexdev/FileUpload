57e6181548be049a28ea09b9cc6d9fb449a2711f
Immanuel Mruk <immanuelmruk@gmail.com>
2024-09-19 17:59:35 +0200

    Implement file upload & download with password

--- app.py
+++ app.py
@@ -9,12 +9,70 @@ load_dotenv()
 app = Flask(__name__)
 app.secret_key = os.getenv('FLASK_SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SEARCH_ENGINE_INDEXING'] = os.getenv('SEARCH_ENGINE_INDEXING', 'False').lower() in ['true', '1', 't']
+app.config['UPLOAD_FOLDER'] = 'uploads'
 
 db.init_app(app)
+
+
+@app.before_request
+def disable_indexing():
+    if app.config['SEARCH_ENGINE_INDEXING']:
+        @app.after_request
+        def add_header(response):
+            response.headers['X-Robots-Tag'] = 'noindex, nofollow'
+            return response
+
+
+@app.route('/')
+def index():
+    return redirect(url_for('upload'))
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        file = request.files['file']
+        password = request.form['password']
+        info = request.form['info']
+
+        if file and password:
+            filename = secure_filename(file.filename)
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(filepath)
+
+            new_file = File(filename=filename, info=info)
+            new_file.set_password(password)
+            db.session.add(new_file)
+            db.session.commit()
+
+            flash('File uploaded successfully!')
+            return redirect(url_for('upload'))
+
+    return render_template('upload.html')
+
+
+@app.route('/file/<filename>', methods=['GET', 'POST'])
+def download(filename):
+    file_entry = File.query.filter_by(filename=filename).first()
+
+    if not file_entry:
+        return "File not found!", 404
+
+    if request.method == 'POST':
+        password = request.form['password']
+
+        if file_entry.check_password(password):
+            return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
+        else:
+            flash('Incorrect password!')
+
+    return render_template('download.html', filename=filename, info=file_entry.info)
 
 
 if __name__ == '__main__':
     with app.app_context():
         if not os.path.exists(app.config['UPLOAD_FOLDER']):
             os.makedirs(app.config['UPLOAD_FOLDER'])
+        db.create_all()
     app.run(debug=os.getenv('DEBUG'), host='0.0.0.0', port=os.getenv('PORT'))
---
+++ models.py
@@ -0,0 +1,41 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+from werkzeug.security import generate_password_hash, check_password_hash
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+class File(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    info = db.Column(db.String(200))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+if __name__ == '__main__':
+    db.create_all()
